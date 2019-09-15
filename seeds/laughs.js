e011745cb8218ed088108a19d0cfece01c90d6a4
Adrian <adrianivsdk@gmail.com>
2019-09-15 15:06:50 -0400

    Automatic deploy is working, trying to figure out other stuff now

--- seeds/laughs.js
+++ seeds/laughs.js
@@ -4,31 +4,37 @@ require('dotenv').config();
 
 const Laughs = [{
     hsl: "LMAOOOAHUEISKALSM",
+    wsl: "When she realized that i'm actually doing this",
     time: "11:53PM",
     date: "09/14/2019"
 }, 
 {
     hsl: "That sticker pack you've sent me is some of the most cursed shit I've seen today",
+    wsl: "Memes that shouldn't exist that I sent to her on Telegram",
     time: "12:26AM",
     date: "09/11/2019",
 },
 {
     hsl: "Laughed at 'stop screaming at me'",
+    wsl: "She respectfully corrected my spanish, I pretended to be offended and said 'stop screaming at me', she laughed",
     time: "12:25AM",
     date: "09/11/2019",
 },
 {
     hsl: "Laughed at 'well, we won't have 50 kitties in the touch of a button",
+    wsl: "Apparently she's a creature of habit, so we have to keep texting instead of using telegram to talk. No one is perfect",
     time: "12:27AM",
     date: "09/11/2019",
 },
 {
     hsl: "Laughed at 'After I spent an hour going through or messages and typing every time you laughed at a dumb joke I make",
+    wsl: "She found it funny that I'm this cute",
     time: "11:54PM",
     date: "09/14/2019",
 },
 {
     hsl: "",
+    wsl: "",
     time: "",
     date: "",
 },
